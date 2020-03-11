72b7a3d9853b5c1d7c706614e3b4e4a6bef51733
bolotrush <arkan.mag@gmail.com>
2020-03-11 13:30:06 +0300

    inverted index is created

---
+++ homework1/src/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+const minWordLength = 2
+
+var (
+	stopWORDS = []string{
+		"",
+		" ",
+		"a",
+		"an",
+		"the",
+	}
+	invertedIndexMap = make(map[string][]string)
+)
+
+func main() {
+
+	setFileToText("homework1/books")
+	/*	for key, _ := range invertedIndexMap {
+			fmt.Println(key + ": " + strconv.Itoa(utf8.RuneCountInString(key)))
+		}
+	*/
+	writeMapToFile(invertedIndexMap)
+}
+
+func setFileToText(path string) {
+
+	files, err := ioutil.ReadDir(path)
+	checkError(err)
+
+	for _, file := range files {
+
+		text, err := ioutil.ReadFile(path + "/" + file.Name())
+		checkError(err)
+
+		regularText := regexp.MustCompile(`[\W]+`).Split(string(text), -1)
+		invertIndex(regularText, strings.TrimRight(file.Name(), ".txt"))
+	}
+}
+
+func writeMapToFile(inputMap map[string][]string) {
+
+	file, err := os.Create("out.txt")
+	checkError(err)
+	defer file.Close()
+
+	for key, value := range inputMap {
+		_, err := file.WriteString(key + ": " + "{" + strings.Join(value, ",") + "}\n")
+		checkError(err)
+	}
+}
+
+func invertIndex(inputWords []string, docId string) /*[]string*/ {
+
+	inputWords = cleanText(inputWords)
+	for _, word := range inputWords {
+		if !isStringInSlice(docId, invertedIndexMap[word]) {
+			invertedIndexMap[word] = append(invertedIndexMap[word], docId)
+		}
+	}
+}
+
+func cleanText(inputWords []string) []string {
+
+	cleanWords := make([]string, 0)
+	for _, word := range inputWords {
+
+		if isStringInSlice(word, stopWORDS) || utf8.RuneCountInString(word) < minWordLength {
+			continue
+		}
+		word = strings.ToLower(word)
+		cleanWords = append(cleanWords, word)
+	}
+	return cleanWords
+}
+
+func isStringInSlice(a string, list []string) bool {
+
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func checkError(err error) {
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
